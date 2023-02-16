44dae1f44955dcba2b96626136c91eec7586ca09
Paulo Vinicius Cristaldo A <102568081+Paulo-vca@users.noreply.github.com>
2023-02-15 22:46:50 -0400

    Add files via upload

--- paulolp2/src/main/java/br/com/ifms/lp2/javabeans/Aluno.java
+++ paulolp2/src/main/java/br/com/ifms/lp2/javabeans/Aluno.java
@@ -2,10 +2,11 @@ package br.com.ifms.lp2.javabeans;
 
 import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
-
+@Entity
 public class Aluno {
 	
 	@Id
